72641d3ef792d2dc00a25b179fe3c4e24cf23711
Dillon Erhardt <dillon.erhardt@gmail.com>
2020-01-11 10:20:23 +0000

    Add netlify

--- links/dev-ops.js
+++ links/dev-ops.js
@@ -2,26 +2,31 @@ export default [
   {
     name: "Zeit",
     url: "https://zeit.co/dashboard",
-    icon: "/icons/zeit.svg",
+    icon: "/icons/zeit.svg"
   },
   {
     name: "Digital Ocean",
     url: "https://www.digitalocean.com/",
-    icon: "/icons/do.png",
+    icon: "/icons/do.png"
   },
   {
     name: "Expo",
     url: "https://expo.io/dashboard",
-    icon: "/icons/expo.svg",
+    icon: "/icons/expo.svg"
   },
   {
     name: "Bitbucket",
     url: "https://bitbucket.org",
-    icon: "/icons/bitbucket.png",
+    icon: "/icons/bitbucket.png"
   },
   {
     name: "GitHub",
     url: "https://github.com",
-    icon: "/icons/github.png",
+    icon: "/icons/github.png"
+  },
+  {
+    name: "Netlify",
+    url: "https://app.netlify.com/",
+    icon: "/icons/netlify.png"
   }
-]
+];
