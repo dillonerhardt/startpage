d7cdc9a216539f90c4435eb2f3f15ff71d6ea7a0
Dillon Erhardt <dillon.erhardt@gmail.com>
2020-03-15 18:01:28 +0000

    Add devhints site

--- links/documentation.js
+++ links/documentation.js
@@ -1,27 +1,32 @@
 export default [
   {
+    name: "Dev Hints",
+    url: "https://devhints.io/",
+    icon: "/icons/devhints.png"
+  },
+  {
     name: "Next.js",
     url: "https://nextjs.org/docs",
-    icon: "/icons/nextjs.svg",
+    icon: "/icons/nextjs.svg"
   },
   {
     name: "Expo",
     url: "https://docs.expo.io/",
-    icon: "/icons/expo.svg",
+    icon: "/icons/expo.svg"
   },
   {
     name: "React",
     url: "https://reactjs.org/docs/getting-started.html",
-    icon: "/icons/react.png",
+    icon: "/icons/react.png"
   },
   {
     name: "Redux",
     url: "https://redux.js.org",
-    icon: "/icons/redux.png",
+    icon: "/icons/redux.png"
   },
   {
     name: "React Native",
     url: "https://facebook.github.io/react-native/",
-    icon: "/icons/react.png",
+    icon: "/icons/react.png"
   }
-]
+];
