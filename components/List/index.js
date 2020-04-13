f1a1b9e9bff8c570f3d790525641a8fb276c4a5c
Dillon Erhardt <dillon.erhardt@gmail.com>
2020-04-13 17:34:14 +0100

    Update dependencies, add key to list items

--- components/List/index.js
+++ components/List/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
-import ListItem from './components/ListItem'
+import React from "react";
+import styled from "styled-components";
+import ListItem from "./components/ListItem";
 
 const List = styled.ul`
   list-style: none;
@@ -13,11 +13,15 @@ const Title = styled.h2`
   font-size: 20px;
 `;
 
-export default ({title, items}) => (
+export default ({ title, items }) => (
   <div>
     <Title>{title}</Title>
-    {items.length > 0 && <List>
-      {items.map(i => <ListItem {...i} />)}
-    </List>}
+    {items.length > 0 && (
+      <List>
+        {items.map((i) => (
+          <ListItem key={i.name} {...i} />
+        ))}
+      </List>
+    )}
   </div>
-)
+);
